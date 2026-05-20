bd496534196767f4f062ba175fde2bd1ddf6ca53
Nicolasdev25 <nicolasleao.dev@gmail.com>
2026-05-20 20:34:04 -0300

    Aula 1 Curso de JavaScript

---
+++ Aula 01/01.js
@@ -0,0 +1,33 @@
+// Variável
+let nome = "Lucas";
+let idade = 18;
+let altura = 1.78;
+
+// Saída de Dados
+
+console.log("nome: ", nome);
+console.log("idade: "+ idade);
+console.log("altura: ", altura);
+
+// Tipagem - não tipada
+
+let valor;
+
+console.log("Valor: ", valor, "| Tipo: ", typeof(valor));
+
+valor = 16;
+
+console.log("Valor: ", valor, "| Tipo: ", typeof(valor));
+
+valor = "Lucas";
+
+console.log("Valor: ", valor, "| Tipo: ", typeof(valor));
+
+valor = true;
+
+
+console.log("Valor: ", valor, "| Tipo: ", typeof(valor));
+
+valor = null;
+
+console.log("Valor: ", valor, "| Tipo: ", typeof(valor));
---
+++ Aula 01/02.js
@@ -0,0 +1,13 @@
+let nome;
+let idade;
+let altura;
+
+// Entrada de Dados (Navegador)
+nome = prompt("Informe seu nome");
+idade = prompt("Informe sua idade");
+altura = prompt("Informe sua altura");
+
+// Saída de Dados (Navegador)
+alert("nome: " + nome);
+alert("idade: " + idade);
+alert("Altura: " + altura);
